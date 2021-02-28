5990efa4a52560cdfc734a03a62965c00b281704
Bill Michaels <williamcarlmichaels@gmail.com>
2021-02-28 16:27:14 -0500

    Finished V1 of writing round with hole results to db from api

--- HandicapTrackerApp/HandicapTrackerAPI/DAL/GolfRoundSqlDAO.cs
+++ HandicapTrackerApp/HandicapTrackerAPI/DAL/GolfRoundSqlDAO.cs
@@ -23,6 +23,8 @@ namespace HandicapTrackerAPI.DAL
                                                         WHERE gr.GolfRoundId = @@IDENTITY;";
         private const string SQL_GET_GOLF_ROUND_BY_ID = "SELECT * FROM GolfRound WHERE GolfRoundId = @golfRoundId";
         private const string SQL_UPDATE_HANDICAP = "UPDATE Player SET Handicap = @handicap WHERE PlayerId = @playerId;";
+        private const string SQL_CREATE_HOLE_RESULTS = @"INSERT INTO HoleResult (GolfRoundId, HoleId, Score, HitFairway, Putts, InGreensideBunker, OutOfBounds, InWater, DropOrOther) VALUES
+                                                          (@golfRoundId, @holeId, @score, @hitFairway, @putts, @inGreensideBunker, @outOfBounds, @inWater, @dropOrOther);";
 
         public List<GolfRound> GetGolfRoundsByPlayerId(int playerId)
         {
@@ -103,6 +105,7 @@ namespace HandicapTrackerAPI.DAL
                     cmd.Parameters.AddWithValue("@datePlayed", round.DatePlayed);
                     cmd.Parameters.AddWithValue("@score", round.Score);
 
+                 
                     SqlDataReader rdr = cmd.ExecuteReader();
 
                     if (rdr.Read())
@@ -115,6 +118,16 @@ namespace HandicapTrackerAPI.DAL
 
                     }
 
+                    rdr.Close();
+                   
+
+                    if (round.HoleResults.Count > 0)
+                    {
+                        CreateHoleResults(conn, round, createdRound.GolfRoundId);
+
+
+                    }
+
                     return createdRound;
                 }
             }
@@ -122,6 +135,26 @@ namespace HandicapTrackerAPI.DAL
             {
 
                 throw;
+            }
+        }
+
+        private void CreateHoleResults(SqlConnection conn, GolfRound round, int roundId)
+        {
+            for (int i = 0; i < 18; i++)
+            {
+
+                SqlCommand cmd = new SqlCommand(SQL_CREATE_HOLE_RESULTS, conn);
+                cmd.Parameters.AddWithValue("@golfRoundId", roundId);
+                cmd.Parameters.AddWithValue("@holeId", round.Tee.Course.Holes[i].HoleId);
+                cmd.Parameters.AddWithValue("@score", round.HoleResults[i].Score);
+                cmd.Parameters.AddWithValue("@hitFairway", round.HoleResults[i].HitFairway);
+                cmd.Parameters.AddWithValue("@putts", round.HoleResults[i].Putts);
+                cmd.Parameters.AddWithValue("@inGreensideBunker", round.HoleResults[i].InGreensideBunker);
+                cmd.Parameters.AddWithValue("@outOfBounds", round.HoleResults[i].OutOfBounds);
+                cmd.Parameters.AddWithValue("@inWater", round.HoleResults[i].InWater);
+                cmd.Parameters.AddWithValue("@dropOrOther", round.HoleResults[i].DropOrOther);
+
+                cmd.ExecuteNonQuery();
             }
         }
 
